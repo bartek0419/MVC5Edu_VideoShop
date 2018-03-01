e2ae5d8c79a9fbea0377ed0b7326e7ba24934d9b
Bartlomiej Balazy <bartlomiej.balazy@ihsmarkit.com>
2018-03-01 14:17:29 +0100

    Add Customer Controler

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoShop.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        private static List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Nowak"},
+                new Customer {Id = 2, Name = "Kowalsky"}
+            };
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
