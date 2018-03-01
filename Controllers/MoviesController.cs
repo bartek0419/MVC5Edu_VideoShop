11f46178a44e89f4ea0171ee8c148a6da84fdefb
Bartlomiej Balazy <bartlomiej.balazy@ihsmarkit.com>
2018-03-01 12:16:45 +0100

    Fix gitignore and add Movies Controler and View

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoShop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private static List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Name = "Shrek", Id = 1},
+                new Movie {Name = "Star War", Id = 2}
+            };
+        }
+    }
+
+    public class Movie
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
