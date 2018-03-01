e51b9f571aed7a585a727e2fa03155abd6e84143
Bartlomiej Balazy <bartlomiej.balazy@ihsmarkit.com>
2018-03-01 14:53:56 +0100

    Add MembershipType and IsSubscribe prop to Customer

---
+++ Migrations/201803011351589_Add_MembershipType_to_CustomersTable.cs
@@ -0,0 +1,34 @@
+namespace VideoShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_MembershipType_to_CustomersTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "MembershipTypeId", c => c.Byte());
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "MembershipTypeId");
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,15 @@
-﻿namespace VideoShop.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
+
+namespace VideoShop.Models
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        [ForeignKey("MembershipTypeId")]
+        public MembershipType MembershipType { get; set; }
+        public byte? MembershipTypeId { get; set; }
     }
 }
