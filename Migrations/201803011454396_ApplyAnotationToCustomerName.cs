e0318c2d0ff7967af2b2e9edc4c870dadb4f85f6
Bartlomiej Balazy <bartlomiej.balazy@ihsmarkit.com>
2018-03-01 15:56:44 +0100

    Add required and length annotation to customer name

---
+++ Migrations/201803011454396_ApplyAnotationToCustomerName.cs
@@ -0,0 +1,18 @@
+namespace VideoShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnotationToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Permissions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VideoShop.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         [ForeignKey("MembershipTypeId")]
