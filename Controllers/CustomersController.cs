f53e9d68dd9f29d5a75f075298e2fa1158a3a74f
Bartlomiej Balazy <bartlomiej.balazy@ihsmarkit.com>
2018-03-06 16:16:39 +0100

    Add Name prop to MembershipType

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using VideoShop.Models;
 
@@ -17,7 +18,7 @@ namespace VideoShop.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
 
             return View(customers);
         }
